66c6a59657804242e63521ad8d8a54b2b529c157
Vivek Lokhande <vivek2811999@gmail.com>
2021-04-22 09:30:19 +0530

    added delete button

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,19 @@
 const BlogList=(props)=>{
+    
     let blogs = props.blogs;
+    let setBlogs = props.setBlogs;
+    
+    const onClickDelete=(id)=>{
+        const newBlogs = blogs.filter(blog=>blog.id!==id);
+        setBlogs(newBlogs);
+    }
     return(
         <div className="blog">
             { blogs.map((blog)=>(
                 <div className="blog-preview">
                     <h1>{blog.title}</h1>
                     <p>written by {blog.author}</p>
+                    <button className="btn-delete" onClick={()=>(onClickDelete(blog.id))}>delete</button>
                 </div>
             ))}
         </div>
